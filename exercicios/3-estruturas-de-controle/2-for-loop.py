62faeed9e61a1217c211849c6e9b935e2c43efd6
VihKun <61951067+VihKun@users.noreply.github.com>
2024-10-21 23:41:53 +0000

    resolucao for loop

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,12 @@
 # Crie uma lista
-
+lista = [ 4, 5, 2, 6, 8, 0 ]
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for elemento in lista:
+  print(elemento)
 
 # Crie um objeto iterável utilizando a função range()
-
+objeto = range(20, 30)
 
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+for x in objeto:
+  print('Estou aprendendo uma linguagem de programação.')
