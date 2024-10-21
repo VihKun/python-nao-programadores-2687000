11e1f3b9adca7134639484e760ecdc99f4026b09
VihKun <61951067+VihKun@users.noreply.github.com>
2024-10-21 22:43:47 +0000

    resolucao lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,14 @@
 # Crie uma lista apenas com elementos numéricos
-
+lista_num = [ 1, 2, 3, 4, 5, 6 ]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+lista_all = [ 1, 'Rita', [ 1, 2, 3 ], 4.0, True]
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print(lista_num[0:5])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print(lista_num[0:-1:2])
 # Remova da lista o último item
-
+lista_num.pop()
 # Insira na lista um novo item
-
+lista_num.append('test')
 # Remova da lista um item específico
+lista_num.remove(3)
