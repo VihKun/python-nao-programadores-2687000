8084dd55bcd554db44c03c9c845156558d3927aa
VihKun <61951067+VihKun@users.noreply.github.com>
2024-10-21 17:13:48 +0000

    resolucao variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Margarida'
 
 # Declare variáveis de cada tipo numérico
-
+x = 2
 
 # Declare uma variável do tipo booleano
+estado = True
