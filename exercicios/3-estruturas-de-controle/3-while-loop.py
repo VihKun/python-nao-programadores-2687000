ed75e9c3ee55d651c4bf6a05161191fd9502192e
VihKun <61951067+VihKun@users.noreply.github.com>
2024-10-21 23:49:17 +0000

    resolucao while loop

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,11 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
+atual = 2
 
+final = 6
 
 # Crie um while loop que imprima na tela o nível atual
-
+while atual <= final:
+  print(atual)
+  atual += 1
 
 # Insira "else" no while loop anterior.
